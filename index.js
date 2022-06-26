ff473536cc278cf3e48e1675f2363aca1232a598
melekarslan13 <melekarslan.ie@gmail.com>
2022-06-26 15:24:46 +0300

    assignment is done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arrayOfNames, string) {
+    return arrayOfNames.filter(
+      (name) => name.toLowerCase() === string.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, letter) {
+    return source.filter(
+      (name) =>
+        name.toLowerCase().indexOf(letter.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(array, soughtName) {
+    return array.filter((record) => record.name === soughtName);
+  }
